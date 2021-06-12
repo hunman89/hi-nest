3f82282cc74da6c258a9c2b724c75763ac642c9c
hunman89 <hunman89@gmail.com>
2021-06-12 17:36:53 +0900

    add. exception

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/Movie.entity';
 
 @Injectable()
@@ -8,11 +8,17 @@ export class MoviesService {
   getAll(): Movie[] {
     return this.movies;
   }
+
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
 
   deleteOne(id: string): boolean {
+    this.getOne(id);
     this.movies.filter((movie) => movie.id !== +id);
     return true;
   }
