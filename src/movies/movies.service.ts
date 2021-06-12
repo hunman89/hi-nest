b9b9954a119d8d450368cd26f3fa3b0440f873b8
hunman89 <hunman89@gmail.com>
2021-06-12 17:43:55 +0900

    change. update, delete

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -19,7 +19,7 @@ export class MoviesService {
 
   deleteOne(id: string): boolean {
     this.getOne(id);
-    this.movies.filter((movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
     return true;
   }
 
@@ -28,5 +28,11 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
